6dc9ce88e5e5533fdc36dfedb0803091352e70b6
Varrience <105261606+Varrience@users.noreply.github.com>
2025-01-25 16:58:59 -0500

    better test run

--- index.js
+++ index.js
@@ -1,3 +1,3 @@
-document.body.write(`
-<video src="sample.mp4" autoplay="" loop="" style="position: fixed;top: 0px;left: 0px;width: 100%;height: 100%;"></video>
-`)
+document.body.innerHTML = `
+<video src="sample.mp4" autoplay muted loop style="position: fixed; top: 0px; left: 0px; width: 100%; height: 100%;"></video>
+`;
